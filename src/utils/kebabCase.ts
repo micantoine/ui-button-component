0032b7cdb7ffbcf0ae1279df76c2de585bf5b7e6
Michael Vurchio <micantoine.dev@gmail.com>
2022-03-30 01:41:43 +0900

    returns string from kebabcase

--- src/utils/kebabCase.ts
+++ src/utils/kebabCase.ts
@@ -3,6 +3,6 @@
  * @param [str] the string to be processed
  * @returns the kebab-case version
  */
-export const kebabCase = (str?: string): string | undefined => {
-  return str?.trim().replace(/((?<=[a-z\d])[A-Z]|(?<=[A-Z\d])[A-Z](?=[a-z]))/g, '-$1').replace(/[\s_+,?:!/\\]/g, '-').toLowerCase();
+export const kebabCase = (str: string): string => {
+  return str.trim().replace(/((?<=[a-z\d])[A-Z]|(?<=[A-Z\d])[A-Z](?=[a-z]))/g, '-$1').replace(/[\s_+,?:!/\\]/g, '-').toLowerCase();
 }
