324dd2ed867f7e63c0a36346b24cbdb95c13a9e8
Michael Vurchio <micantoine.dev@gmail.com>
2022-03-29 21:35:55 +0900

    Use memo on breadcrumb

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,14 +1,11 @@
-import { type FC } from 'react';
+import { useMemo, type FC } from 'react';
 import { Link, useLocation } from "react-router-dom";
 import { getRoute, type Route } from '../routes';
 import styles from './Breadcrumb.module.css';
 
-const Breadcrumb: FC = () => {
-  const location = useLocation();
+const getBreadcrumb = (pathname: string): Route[] => {
   const breadcrumb: Route[] = [];
-
-  let url = `${location.pathname}/`;
-
+  let url = `${pathname}/`;
   while (url !== '') {
     url = url.substring(0, url.lastIndexOf('/'));
     const route = getRoute(url);
@@ -16,7 +13,15 @@ const Breadcrumb: FC = () => {
       breadcrumb.unshift(route);
     }
   }
-  
+  return breadcrumb;
+}
+
+const Breadcrumb: FC = () => {
+  const location = useLocation();
+  const breadcrumb = useMemo<Route[]>(
+    () => getBreadcrumb(location.pathname)
+  , [location.pathname]);
+
   return (
     <ul className={styles.list}>
       {breadcrumb.map((route, index) =>
