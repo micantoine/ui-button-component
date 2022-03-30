0cc39549651aaae28e30bafbf4e8c9e6b7957bf2
Michael Vurchio <micantoine.dev@gmail.com>
2022-03-30 22:23:29 +0900

    Update on visibility on change

--- src/components/VisibilitySwitcher.tsx
+++ src/components/VisibilitySwitcher.tsx
@@ -1,4 +1,4 @@
-import { type FC } from 'react';
+import { useEffect, useState, type FC } from 'react';
 import { Button, Icon } from './';
 import HiddenIcon from '../assets/visibility-hidden.svg'; 
 import VisibleIcon from '../assets/visibility-visible.svg'; 
@@ -8,14 +8,20 @@ const VisibilitySwitcher: FC<{
   className?: string,
   onChange: (status: boolean) => void
 }> = ({ show, onChange, className }) => {
+  const [visible, setVisibility] = useState(show);
 
   const handleClick = (): void => {
-    onChange(!show);
+    setVisibility(!visible);
+    onChange(!visible);
   }
+
+  useEffect(() => {
+    setVisibility(show);
+  }, [show]);
 
   return (
     <Button variant="icon" className={className} onClick={handleClick}>
-      <Icon src={show ? VisibleIcon : HiddenIcon} />
+      <Icon src={visible ? VisibleIcon : HiddenIcon} />
     </Button>
   );
 }
