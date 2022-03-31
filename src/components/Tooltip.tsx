3cbd80ac38e6d1824874d9a2dd9104423718cdbe
Michael Vurchio <micantoine.dev@gmail.com>
2022-03-31 10:02:00 +0900

    Style home page

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -2,93 +2,92 @@ import { useState, useEffect, useCallback, useRef, type FC, type CSSProperties }
 import styles from './Tooltip.module.css';
 
 const Tooltip: FC<{ info: string | JSX.Element }> = (props) => {
-    const el = useRef<HTMLElement>(null);
-    const infoEl = useRef<HTMLElement>(null);
-    const arrowEl = useRef<HTMLElement>(null);
+  const el = useRef<HTMLElement>(null);
+  const infoEl = useRef<HTMLElement>(null);
+  const arrowEl = useRef<HTMLElement>(null);
 
-    const [arrowStyle, setArrowStyle] = useState<CSSProperties>({});
-    const [infoStyle, setInfoStyle] = useState<CSSProperties>({});
+  const [arrowStyle, setArrowStyle] = useState<CSSProperties>({});
+  const [infoStyle, setInfoStyle] = useState<CSSProperties>({});
 
-    const hide = useCallback(() => {
-      setInfoStyle((state) => ({...state, display: 'none'}));
-      setArrowStyle((state) => ({...state, display: 'none'}));
-    }, []);
+  const hide = useCallback(() => {
+    setInfoStyle((state) => ({...state, display: 'none'}));
+    setArrowStyle((state) => ({...state, display: 'none'}));
+  }, []);
 
-    const offset = useCallback((): {top: number; left: number} =>{
-      if (!infoEl.current) {
-        return { top:0, left: 0}
-      }
-      const rect = infoEl.current.getBoundingClientRect();
-      return {
-        top: rect.top + document.body.scrollTop,
-        left: rect.left + document.body.scrollLeft
-      }
-    }, []);
+  const offset = useCallback((): {top: number; left: number} =>{
+    if (!infoEl.current) {
+      return { top:0, left: 0}
+    }
+    const rect = infoEl.current.getBoundingClientRect();
+    return {
+      top: rect.top + document.body.scrollTop,
+      left: rect.left + document.body.scrollLeft
+    }
+  }, []);
 
-    const alignToLeftInner = useCallback(() => {
-      const offsetLeft = offset().left;
+  const alignToLeftInner = useCallback(() => {
+    const offsetLeft = offset().left;
+    setInfoStyle((state) => ({
+      ...state,
+      width: `${document.body.offsetWidth}px`,
+      marginLeft: `-${offsetLeft}px`
+    })); 
+  },[offset]);
+
+  const alignToLeft = useCallback(() => {
+    const offsetLeft = offset().left;
+    if (document.body.offsetLeft > offsetLeft) {
       setInfoStyle((state) => ({
         ...state,
-        width: `${document.body.offsetWidth}px`,
-        marginLeft: `-${offsetLeft}px`
-      })); 
-    },[offset]);
-
-    const alignToLeft = useCallback(() => {
-      const offsetLeft = offset().left;
-      if (document.body.offsetLeft > offsetLeft) {
-        setInfoStyle((state) => ({
-          ...state,
-          left: 0,
-          right: 'auto',
-        }));
-        alignToLeftInner();
-      }
-    }, [alignToLeftInner, offset]);
-
-    const adjustPosition = useCallback(async () => {
-      setArrowStyle((state) => ({
-        ...state,
-        display: 'block'
+        left: 0,
+        right: 'auto',
       }));
+      alignToLeftInner();
+    }
+  }, [alignToLeftInner, offset]);
+
+  const adjustPosition = useCallback(async () => {
+    setArrowStyle((state) => ({
+      ...state,
+      display: 'block'
+    }));
+    setInfoStyle((state) => ({
+      ...state,
+      display: 'block',
+    }));
+
+    if (document.body.offsetLeft > offset().left) {
       setInfoStyle((state) => ({
         ...state,
-        display: 'block',
+        left: '0',
+        transform: 'none',
       }));
 
-      if (document.body.offsetLeft > offset().left) {
-        setInfoStyle((state) => ({
-          ...state,
-          left: '0',
-          transform: 'none',
-        }));
-
-        if (document.body.offsetWidth < (offset().left + (infoEl.current?.offsetWidth ?? 0))) {
-          alignToLeftInner()
-        }
-      } else if (document.body.offsetWidth < (offset().left +(infoEl.current?.offsetWidth ?? 0))) {
-        setInfoStyle((state) => ({
-          ...state,
-          right: '0',
-          left: 'auto',
-          transform: 'none',
-        }));
-
-        alignToLeft();
+      if (document.body.offsetWidth < (offset().left + (infoEl.current?.offsetWidth ?? 0))) {
+        alignToLeftInner()
       }
-    }, [alignToLeft, alignToLeftInner, offset]);
+    } else if (document.body.offsetWidth < (offset().left +(infoEl.current?.offsetWidth ?? 0))) {
+      setInfoStyle((state) => ({
+        ...state,
+        right: '0',
+        left: 'auto',
+        transform: 'none',
+      }));
 
-    useEffect(() => {
-      const currentEl = el.current;
-      currentEl?.addEventListener('mouseenter', adjustPosition);
-      currentEl?.addEventListener('mouseleave', hide);
+      alignToLeft();
+    }
+  }, [alignToLeft, alignToLeftInner, offset]);
 
-      return () => {
-        currentEl?.removeEventListener('mouseenter', adjustPosition);
-        currentEl?.removeEventListener('mouseleave', hide);
-      }
-    });
+  useEffect(() => {
+    const currentEl = el.current;
+    currentEl?.addEventListener('mouseenter', adjustPosition);
+    currentEl?.addEventListener('mouseleave', hide);
 
+    return () => {
+      currentEl?.removeEventListener('mouseenter', adjustPosition);
+      currentEl?.removeEventListener('mouseleave', hide);
+    }
+  });
 
   return (
     <span
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,22 @@
 import { type FC } from 'react';
 import { Link } from "react-router-dom";
+import { MainContent } from '../components';
 import { routes, RouteTypes } from '../routes';
+
+import styles from './Home.module.css';
 
 const Home: FC = () => {
   return (
-    <>
-      <h1>{routes[RouteTypes.HOME].title}</h1>
-      <ul>
+    <MainContent>
+      <h1 className={styles.title}>{routes[RouteTypes.HOME].title}</h1>
+      <ul className={styles.list}>
         <li>
-          <Link to={routes[RouteTypes.MATERIAL_UI].path}>
+          <Link className={styles.item} to={routes[RouteTypes.MATERIAL_UI].path}>
             {routes[RouteTypes.MATERIAL_UI].title}
           </Link>
         </li>
       </ul>
-    </>
+    </MainContent>
   );
 }
 
